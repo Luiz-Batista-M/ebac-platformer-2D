e6293f6efd10183e729fe2a3af8563d9e1f0396d
Luiz Batista <luizguilhermebmatos@gmail.com>
2025-11-28 19:07:33 -0300

    Add Basic Menu

--- Assets/Scripts/Utils/LoadScene.cs
+++ Assets/Scripts/Utils/LoadScene.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LoadScene : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+ 
+    public void Load(int i)
     {
-        
+        SceneManager.LoadScene(i);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Load(string s)
     {
-        
+        SceneManager.LoadScene(s);
     }
 }
